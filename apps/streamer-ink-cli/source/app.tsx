de83838db779a8c885fa87358d0e0a2a76519558
rifatNR <rifatnoor92@gmail.com>
2025-10-24 02:44:48 +0600

    Creating Internal package working

--- apps/streamer-ink-cli/source/app.tsx
+++ apps/streamer-ink-cli/source/app.tsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text} from 'ink';
 import Example from '@/Example.js';
+import {greet} from 'get-local-info';
 
 type Props = {
 	name: string | undefined;
 };
 
 export default function App({name = 'Stranger'}: Props) {
+	useEffect(() => {
+		console.log(greet('RUSTOM...'));
+	}, []);
+
 	return (
 		<>
 			<Text>
 				Hello, <Text color="green">{name}</Text>
 			</Text>
-			<Example />
+			{/* <Example /> */}
 		</>
 	);
 }
