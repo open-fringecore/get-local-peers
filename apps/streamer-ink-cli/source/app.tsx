322aa79e48a2d729b71cfede376aff00d630211b
rifatNR <rifatnoor92@gmail.com>
2025-10-24 03:27:05 +0600

    doing something

--- apps/streamer-ink-cli/source/app.tsx
+++ apps/streamer-ink-cli/source/app.tsx
@@ -1,16 +1,13 @@
 import React, {useEffect} from 'react';
 import {Text} from 'ink';
 import Example from '@/Example.js';
-import {greet} from 'get-local-info';
 
 type Props = {
 	name: string | undefined;
 };
 
 export default function App({name = 'Stranger'}: Props) {
-	useEffect(() => {
-		console.log(greet('RUSTOM...'));
-	}, []);
+	useEffect(() => {}, []);
 
 	return (
 		<>
--- packages/get-local-info/src/index.ts
+++ packages/get-local-info/src/index.ts
@@ -1,3 +1,73 @@
-export const greet = (name: string) => {
-    return `Yo! brother, ${name}!`;
+import * as os from "os";
+
+const isPrivateIP = (ip: string): boolean => {
+    return ip.startsWith("192.168");
 };
+
+const calculateBroadcastAddress = (ipAddress: string, subnetMask: string) => {
+    const ipBinary = ipAddress
+        .split(".")
+        .map((part) => parseInt(part, 10).toString(2).padStart(8, "0"))
+        .join("");
+    const maskBinary = subnetMask
+        .split(".")
+        .map((part) => parseInt(part, 10).toString(2).padStart(8, "0"))
+        .join("");
+
+    const broadcastBinary = ipBinary
+        .split("")
+        .map(
+            (bit, index) =>
+                parseInt(bit, 2) | (maskBinary[index] === "0" ? 1 : 0)
+        )
+        .join("");
+
+    const broadcastParts = [];
+    for (let i = 0; i < 4; i++) {
+        broadcastParts.push(parseInt(broadcastBinary.substr(i * 8, 8), 2));
+    }
+    return broadcastParts.join(".");
+};
+
+const getLocalIP = (): {
+    address: string | null;
+    subnetMask: string | null;
+    addresses: string[];
+} => {
+    const interfaces = os.networkInterfaces();
+    const addresses: string[] = [];
+    let address = null;
+    let subnetMask = null;
+
+    for (const iface in interfaces) {
+        for (const details of interfaces[iface]!) {
+            if (details.family === "IPv4" && !details.internal) {
+                addresses.push(details.address);
+                if (isPrivateIP(details.address)) {
+                    address = details.address;
+                    subnetMask = details.netmask;
+                }
+            }
+        }
+    }
+
+    return {
+        address,
+        subnetMask,
+        addresses,
+    };
+};
+
+const getLocalInfo = () => {
+    const { address, subnetMask } = getLocalIP();
+
+    if (address && subnetMask) {
+        const broadcastAddress = calculateBroadcastAddress(address, subnetMask);
+
+        return [address, broadcastAddress];
+    }
+
+    return;
+};
+
+export default getLocalInfo;
