41b39c23e34ee7b22d2f10ee92322e6716dd706d
rifatNR <rifatnoor92@gmail.com>
2025-10-24 17:07:04 +0600

    Added some code

---
+++ packages/get-local-peers/src/lib/helper/broadcast.ts
@@ -0,0 +1,25 @@
+import { Socket } from "dgram";
+
+const sendBroadcast = (
+    server: Socket,
+    ADDRESS: string,
+    PORT: number,
+    msg: string
+): void => {
+    const MESSAGE: Buffer = Buffer.from(msg);
+
+    server.send(
+        MESSAGE,
+        0,
+        MESSAGE.length,
+        PORT,
+        ADDRESS,
+        (err: Error | null): void => {
+            if (err) throw err;
+            // console.log(`Broadcast sent to ${ADDRESS}:${PORT}`);
+            // server.close();
+        }
+    );
+};
+
+export default sendBroadcast;
